8531a1d908afe5a0d92110e35126dfde600e54f7
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-08-20 09:47:55 +0700

    Add allowed cors origin

---
+++ src/main/java/com/bank_dki/be_dms/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.bank_dki.be_dms.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("Configuring CORS with allowed origins: {}", (Object) allowedOrigins);
+
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins);
+    }
+}
